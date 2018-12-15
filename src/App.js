47b136cd1b8e905768f09903f61a18a7b533ec6b
Ricardo Gottheil <ricardogottheil@gmail.com>
2018-12-15 18:40:30 -0500

    Creando componentes reutilizablesy mejorando el layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Title } from './components/Title'
 import { SearchForm } from './components/SearchForm'
+import { MoviesList } from './components/MoviesList'
 
 import './App.css';
 import 'bulma/css/bulma.css'
@@ -13,12 +14,7 @@ class App extends Component {
     this.setState({ results })
   }
 
-  _renderResults(){
-    const { results } = this.state
-    return results.map(movie => {
-      return <p key={movie.imdbID}>{movie.Title}</p>
-    })
-  }
+
 
   render() {
     return (
@@ -31,7 +27,7 @@ class App extends Component {
 
           {this.state.results.length === 0 
             ? <p>Sin resultados</p> 
-            : this._renderResults() 
+            : <MoviesList movies={this.state.results} />
           }
       </div>
     );
