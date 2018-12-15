e6c1b950de724bb9ff03bc684c16b83af77a31e1
Ricardo Gottheil <ricardogottheil@gmail.com>
2018-12-15 18:56:05 -0500

    Mejoras en la implementación de búsqueda

--- src/App.js
+++ src/App.js
@@ -8,12 +8,19 @@ import 'bulma/css/bulma.css'
 
 class App extends Component {
 
-  state = { results: [] }
+  state = { usedSearch: false, results: [] }
   
   _handleResults = (results) => {
-    this.setState({ results })
+    this.setState({ results, usedSearch: true })
   }
 
+  _renderResults () {
+    
+    return  this.state.results.length === 0
+      ? <p>Sorry! Results not found!</p>
+      : <MoviesList movies={this.state.results} />
+    
+  }
 
 
   render() {
@@ -24,11 +31,11 @@ class App extends Component {
           <SearchForm onResults={this._handleResults} />
         </div>
 
+        {this.state.usedSearch
+          ? this._renderResults()
+          : <small>Use the form to search a movie</small>}
 
-          {this.state.results.length === 0 
-            ? <p>Sin resultados</p> 
-            : <MoviesList movies={this.state.results} />
-          }
+
       </div>
     );
   }
